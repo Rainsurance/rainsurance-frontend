e2e66253cf74cf2f6b7fdcd02a2b4019868e0d09
André Teves <teves@webeleven.com.br>
2023-05-16 15:12:39 -0300

    rainbowkit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-// next.config.js
-module.exports = {
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
-}
+};
+
+module.exports = nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,49 @@
 import Theme from '@/styles/theme';
 import '../styles/overlayscrollbars.css';
 
-export default function App({ Component, pageProps }) {
+// RainbowKit imports
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { arbitrum, goerli, mainnet, optimism, polygon, polygonMumbai } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli, polygonMumbai] : []),
+  ],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Rainsurance DApp',
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID,
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+function App({ Component, pageProps }) {
   return (
     <>
-      <Theme>
-        <Component {...pageProps} />
-      </Theme>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Theme>
+          <Component {...pageProps} />
+        </Theme>
+      </RainbowKitProvider>
+    </WagmiConfig>
     </>
   );
 }
+
+export default App;
