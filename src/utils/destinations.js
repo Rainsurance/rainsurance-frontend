fef0e276bc5b5629adb996aa020a550c35e4ac52
André Teves <teves@webeleven.com.br>
2023-08-14 15:07:45 -0300

    new cities

--- src/utils/destinations.js
+++ src/utils/destinations.js
@@ -1,9 +1,12 @@
 export const destinations = [
     { id: "10001", name: "São Paulo", lat: -23.55062, lng: -46.63437, enabled: false },
-    { id: "10002", name: "Rio de Janeiro", lat: -22.90278, lng: -43.2075, enabled: false },
     { id: "10003", name: "Bertioga", lat: -23.798865, lng: -46.022966, enabled: false },
+    { id: "10101", name: "Buenos Aires", lat: -34.608945, lng: -58.381939, enabled: true },
     { id: "20001", name: "London", lat: 51.508079, lng: -0.127980, enabled: true },
     { id: "30003", name: "Miami", lat: 25.761527, lng: -80.194972, enabled: true },
+    { id: "10002", name: "Rio de Janeiro", lat: -22.90278, lng: -43.2075, enabled: true },
     { id: "20002", name: "Rome", lat: 41.902704, lng: 12.496235, enabled: true },
+    { id: "50101", name: "Seoul", lat: 37.551351, lng: 126.991581, enabled: true },
+    { id: "50201", name: "Tokio", lat: 35.682440, lng: 139.757802, enabled: true },
     { id: "*", name: "Anywhere", lat: null, lng: null, enabled: true, wildcard: true },
 ];
